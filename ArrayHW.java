c218154624f9ab1e2c945b44ae20d22db4a4bc40
HwangEoJun <eojun23@gmail.com>
2016-06-01 00:30:26 +0900

    Comment
    i write a comment for me to understand

--- ArrayHW.java
+++ ArrayHW.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 public class ArrayHW {
 
 	  public static void main(String[] args){   
-		    int y = 1;
-		    int[][] z = new int[2][2];
+		    int y = 1;                     // make variable to fill the array z
+		    int[][] z = new int[2][2];     // make two-dimensional arrays
 		    
 		    
-		    for(int i=0; i<2; i++){
+		    for(int i=0; i<2; i++){        
 		    	for(int j=0; j<2; j++){
-		    		z[i][j]=y;
-		    		y += 1;
+		    		z[i][j]=y;        // in this for loop, i go 0 to 1, j go 0 to 1    
+		    		y += 1;           // at the end of case, raise the value of y
 		    	}
 		    }
 		    System.out.println(Arrays.deepToString(z));
